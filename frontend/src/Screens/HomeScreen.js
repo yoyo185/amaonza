24715e79ee041fa77fb3d1fbcdc6df4668224f3c
Youyou Xie <youyou@scml20dev19.corp.michaels.com>
2020-08-06 23:31:09 -0400

    redux

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,27 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import data from '../data';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { productListReducer } from "../reducers/productReducers";
+import { listProducts } from "../actions/productActions";
 
 function HomeScreen(props) {
-    return <ul className="products">
-        {
-            data.products.map(product =>
-                <li>
-                    <div className="product">
-                        <div className="product-name">
-                            <Link to={'/product/' + product._id}><img className="product-image" src={product.image} alt="product" /></Link>
-                        </div>
+  //   const [products, setProduct] = useState([]);
+  const productList = useSelector((state) => state.productList);
+  const { products, loading, error } = productList;
+  const dispatch = useDispatch();
 
-                        <div className="product-name">
-                            <Link to={'/product/' + product._id}>{product.name}</Link>
-                        </div>
-                        <div className="product-brand">{product.brand}</div>
-                        <div className="product-price">{product.price}</div>
-                        <div className="product-rating">{product.rating} Stars ({product.numReviews}reviews)</div>
-                    </div>
-                </li>)
-        }
+  useEffect(() => {
+    dispatch(listProducts());
 
+    return () => {};
+  }, []);
+
+  console.log(error);
+  return loading ? (
+    <div>Loading...</div>
+  ) : error ? (
+    <div>{error}</div>
+  ) : (
+    <ul className="products">
+      {products.map((product) => (
+        <li key={product._id}>
+          <div className="product">
+            <div className="product-name">
+              <Link to={"/product/" + product._id}>
+                <img
+                  className="product-image"
+                  src={product.image}
+                  alt="product"
+                />
+              </Link>
+            </div>
+
+            <div className="product-name">
+              <Link to={"/product/" + product._id}>{product.name}</Link>
+            </div>
+            <div className="product-brand">{product.brand}</div>
+            <div className="product-price">{product.price}</div>
+            <div className="product-rating">
+              {product.rating} Stars ({product.numReviews}reviews)
+            </div>
+          </div>
+        </li>
+      ))}
     </ul>
+  );
 }
 export default HomeScreen;
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,35 +1,37 @@
-
 export default {
-    products: [
-        {
-            _id: '123',
-            name: 'Slim Shirt',
-            category: 'Shirts',
-            image: '/images/d1.jpg',
-            price: 60,
-            brand: 'Nike',
-            rating: 4.5,
-            numReviews: 10
-        },
-        {
-            _id: '456',
-            name: 'Fit Shirt',
-            category: 'Shirts',
-            image: '/images/d1.jpg',
-            price: 60,
-            brand: 'Nike',
-            rating: 4.5,
-            numReviews: 10
-        },
-        {
-            _id: '789',
-            name: 'Best Pants',
-            category: 'Pants',
-            image: '/images/d1.jpg',
-            price: 60,
-            brand: 'Nike',
-            rating: 4.5,
-            numReviews: 1
-        }
-    ]
-}
+  products: [
+    {
+      _id: "123",
+      name: "Slim Shirt",
+      category: "Shirts",
+      image: "/images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 6,
+    },
+    {
+      _id: "456",
+      name: "Fit Shirt",
+      category: "Shirts",
+      image: "/images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 6,
+    },
+    {
+      _id: "789",
+      name: "Best Pants",
+      category: "Pants",
+      image: "/images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 1,
+      countInStock: 6,
+    },
+  ],
+};
